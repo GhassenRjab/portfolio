e73fa497613e03411f0da02f05d343a75c4fe2d3
Ghassen Rjab <rjab.ghassen@gmail.com>
2019-06-15 20:41:26 +0100

    Update react-images package

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,53 +22,46 @@ import full07 from "../assets/images/fulls/07.jpg";
 
 const DEFAULT_IMAGES = [
   {
-    id: "1",
-    src: full01,
+    source: full01,
     thumbnail: thumb01,
     caption: "Zoe",
     description:
       "Design, set up architecture and develop from scratch Zoe (the chatbot for Quicktext hotels) using Azure Bot Service, Cognitive Services and Node.js."
   },
   {
-    id: "2",
-    src: full02,
+    source: full02,
     thumbnail: thumb02,
     caption: "Paristay",
     description:
       "Full-stack development of Paristay using CodeIgniter. Improve SQL queries and site response time. Develop a dashboard dedicated to yield management."
   },
   {
-    id: "3",
-    src: full03,
+    source: full03,
     thumbnail: thumb03,
     caption: "WIKI.TOTAL",
     description: "Full-stack development of WIKI.TOTAL using CodeIgniter."
   },
   {
-    id: "4",
-    src: full04,
+    source: full04,
     thumbnail: thumb04,
     caption: "Fondation Lenval",
     description: "Front-end development and development of Drupal modules"
   },
   {
-    id: "5",
-    src: full05,
+    source: full05,
     thumbnail: thumb05,
     caption: "Urofrance",
     description: "Front-end development and development of Drupal modules"
   },
   {
-    id: "6",
-    src: full06,
+    source: full06,
     thumbnail: thumb06,
     caption: "SmartServices",
     description:
       "Development and maintenance of SmartServices using Java EE (spring, Hibernate and JSF), a POS Web application for restaurants."
   },
   {
-    id: "7",
-    src: full07,
+    source: full07,
     thumbnail: thumb07,
     caption: "BipSMS",
     description:
@@ -77,50 +70,6 @@ const DEFAULT_IMAGES = [
 ];
 
 class HomeIndex extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      lightboxIsOpen: false,
-      currentImage: 0
-    };
-
-    this.closeLightbox = this.closeLightbox.bind(this);
-    this.gotoNext = this.gotoNext.bind(this);
-    this.gotoPrevious = this.gotoPrevious.bind(this);
-    this.openLightbox = this.openLightbox.bind(this);
-    this.handleClickImage = this.handleClickImage.bind(this);
-  }
-
-  openLightbox(index, event) {
-    event.preventDefault();
-    this.setState({
-      currentImage: index,
-      lightboxIsOpen: true
-    });
-  }
-  closeLightbox() {
-    this.setState({
-      currentImage: 0,
-      lightboxIsOpen: false
-    });
-  }
-  gotoPrevious() {
-    this.setState({
-      currentImage: this.state.currentImage - 1
-    });
-  }
-  gotoNext() {
-    this.setState({
-      currentImage: this.state.currentImage + 1
-    });
-  }
-  handleClickImage() {
-    if (this.state.currentImage === this.props.images.length - 1) return;
-
-    this.gotoNext();
-  }
-
   render() {
     const siteTitle = "Ghassen Rjab";
     const siteDescription =
@@ -149,16 +98,7 @@ class HomeIndex extends React.Component {
           <section id="two">
             <h2>Recent Work</h2>
 
-            <Gallery
-              images={DEFAULT_IMAGES.map(
-                ({ id, src, thumbnail, caption, description }) => ({
-                  src,
-                  thumbnail,
-                  caption,
-                  description
-                })
-              )}
-            />
+            <Gallery images={DEFAULT_IMAGES} />
           </section>
 
           <section id="three">
