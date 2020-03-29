f11c45c8fc1baf8f30c31e5b6efc18e7c021ceee
Ghassen Rjab <rjab.ghassen@gmail.com>
2020-03-29 16:25:10 +0200

    Use React functional components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,66 +1,62 @@
 import React from "react";
 import { OutboundLink } from "gatsby-plugin-google-analytics";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div id="footer">
-        <div className="inner">
-          <ul className="icons">
-            <li>
-              <OutboundLink
-                href="https://twitter.com/GhassenRjab"
-                className="icon fa-twitter"
-                title="Twitter profile"
-                aria-label="Twitter profile"
-              >
-                <span className="label">Twitter</span>
-              </OutboundLink>
-            </li>
-            <li>
-              <OutboundLink
-                href="https://github.com/GhassenRjab"
-                className="icon fa-github"
-                title="GitHub profile"
-                aria-label="GitHub profile"
-              >
-                <span className="label">Github</span>
-              </OutboundLink>
-            </li>
-            <li>
-              <OutboundLink
-                href="https://www.linkedin.com/in/ghassenrjab/"
-                className="icon fa-linkedin"
-                title="LinkedIn profile"
-                aria-label="LinkedIn profile"
-              >
-                <span className="label">LinkedIn</span>
-              </OutboundLink>
-            </li>
-            <li>
-              <a
-                href="mailto:rjab.ghassen@gmail.com"
-                className="icon fa-envelope-o"
-                title="Email address"
-                aria-label="Email address"
-              >
-                <span className="label">Email</span>
-              </a>
-            </li>
-          </ul>
-          <ul className="copyright">
-            <li>&copy; Gatsby Starter Strata</li>
-            <li>
-              Design:{" "}
-              <OutboundLink href="http://html5up.net" title="HTML5 UP">
-                HTML5 UP
-              </OutboundLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
-}
+const Footer = () => (
+  <div id="footer">
+    <div className="inner">
+      <ul className="icons">
+        <li>
+          <OutboundLink
+            href="https://twitter.com/GhassenRjab"
+            className="icon fa-twitter"
+            title="Twitter profile"
+            aria-label="Twitter profile"
+          >
+            <span className="label">Twitter</span>
+          </OutboundLink>
+        </li>
+        <li>
+          <OutboundLink
+            href="https://github.com/GhassenRjab"
+            className="icon fa-github"
+            title="GitHub profile"
+            aria-label="GitHub profile"
+          >
+            <span className="label">Github</span>
+          </OutboundLink>
+        </li>
+        <li>
+          <OutboundLink
+            href="https://www.linkedin.com/in/ghassenrjab/"
+            className="icon fa-linkedin"
+            title="LinkedIn profile"
+            aria-label="LinkedIn profile"
+          >
+            <span className="label">LinkedIn</span>
+          </OutboundLink>
+        </li>
+        <li>
+          <a
+            href="mailto:rjab.ghassen@gmail.com"
+            className="icon fa-envelope-o"
+            title="Email address"
+            aria-label="Email address"
+          >
+            <span className="label">Email</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="copyright">
+        <li>&copy; Gatsby Starter Strata</li>
+        <li>
+          Design:{" "}
+          <OutboundLink href="http://html5up.net" title="HTML5 UP">
+            HTML5 UP
+          </OutboundLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,20 @@ import React from "react";
 import Footer from "./Footer";
 import avatar from "../assets/images/avatar.jpg";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header id="header">
-        <div className="inner">
-          <img src={avatar} className="image avatar" alt="Ghassen Rjab" />
-          <h1>
-            <strong>Hi, I am Ghassen Rjab </strong>
-            <br />
-            Fullstack JavaScript Developer
-            <br />
-            and problem solver
-          </h1>
-        </div>
-        <Footer />
-      </header>
-    );
-  }
-}
+const Header = () => (
+  <header id="header">
+    <div className="inner">
+      <img src={avatar} className="image avatar" alt="Ghassen Rjab" />
+      <h1>
+        <strong>Hi, I am Ghassen Rjab </strong>
+        <br />
+        Fullstack JavaScript Developer
+        <br />
+        and problem solver
+      </h1>
+    </div>
+    <Footer />
+  </header>
+);
 
 export default Header;
